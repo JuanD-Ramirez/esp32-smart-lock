19a4bd3ef716d31e63b4b12a5f5790c94a55f788
JD Ramirez <u1406247@umail.utah.edu>
2025-05-14 16:39:22 -0700

    gave a clear structure to my code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file main.cpp
  * @brief Entry point for ESP32 Smart Lock Project
- * 
+ *
  * Initializes the serial interface and keypad system
  * and continuously reads key presses from keypad.
  */
@@ -14,13 +14,13 @@
 // KeypadManager instance used to access keypad functionality
 KeypadManager keypadMgr;
 
-//DisplayManager instance used to display messages
+// DisplayManager instance used to display messages
 DisplayManager display;
 
 // servo instance used to control lock
 LockController lockMotor(22);
 
-///Password Lenght
+/// Password Lenght
 const int PIN_LENGTH = 4;
 
 /// the currently configured pin
@@ -38,27 +38,164 @@ bool locked = true;
 /// pinstore instance to save pin to EEPROM
 PinStorage pinStore(PIN_LENGTH, 0);
 
+// -----------------------------------------------------------------------------
+// Lockout Security Configuration
+// -----------------------------------------------------------------------------
+
+/// current number of failed attempts
+int failedAttempts = 0;
+
+/// Timestamp in milliseconds when the lockout period started.
+/// Used to calculate how long the system should remain locked out.
+unsigned long lockoutStart = 0;
+
+/// Maximum number of allowed attempts before triggering a lockout.
+const int MAX_FAILED_ATEMPTS = 3;
+
+/// Duration of the lockout period
+const unsigned long LOCKOUT_DURATION_MS = 30000;
+
 /**
  * @brief Unlocks the lock, displays success, and relocks after a delay.
  */
-void unlockLock() {
+void unlockLock()
+{
   lockMotor.unlock();
   display.showMessage("Access Granted");
   delay(3000);
   lockMotor.lock();
 }
 
-void passwordChange() {
-  display.showMessage("Click * to change the pin");
-  
+/**
+ * @brief Checks if the number of failed attemps exceeds the number of maximum test.
+ * If exceceded Lockout the user from being able to input new pin momentarily.
+ */
+void CheckIfLockOut()
+{
+  if (failedAttempts >= MAX_FAILED_ATEMPTS)
+  {
+    unsigned long now = millis();
+
+    // First entry into lockout: initialize timestamp and show message
+    if (lockoutStart == 0)
+    {
+      lockoutStart = now;
+      display.showMessage("LOCKED OUT");
+    }
+
+    // While still within the lockout window, do nothing
+    if (now - lockoutStart < LOCKOUT_DURATION_MS)
+    {
+      return;
+    }
+    else
+    {
+      // Lockout duration finished — reset state
+      failedAttempts = 0;
+      lockoutStart = 0;
+      display.clear();
+    }
+  }
+}
+
+void handlePinEntry(char key)
+{
+  if (pinIndex < PIN_LENGTH)
+  {
+    enteredPin[pinIndex] = key;
+    pinIndex++;
+    enteredPin[pinIndex] = '\0'; // Null-terminate after adding
+    display.showPinProgress(enteredPin);
+  }
+}
+
+void handlePinValidation()
+{
+  enteredPin[PIN_LENGTH] = '\0'; // null terminate string
+
+  // Compare if entered pin is the correct pin
+  if (strcmp(enteredPin, pinStore.getPin()) == 0)
+  {
+    display.showMessage("Access Granted");
+    unlockLock();
+    locked = false;
+  }
+
+  // pin overide option
+  else if (strcmp(enteredPin, "0101") == 0)
+  {
+    display.showMessage("Enter a new pin to be saved");
+    pinIndex = 0;
+
+    // Enter new Pin
+    while (pinIndex < PIN_LENGTH)
+    {
+      char tempKey = keypadMgr.getKey();
+      if (tempKey >= '0' && tempKey <= '9')
+      {
+        enteredPin[pinIndex] = tempKey;
+        pinIndex++;
+        enteredPin[pinIndex] = '\0';
+        display.showPinProgress(enteredPin);
+      }
+      delay(50);
+    }
+
+    delay(500);
+    pinIndex = 0;
+    display.showMessage("Confirm pin");
+
+    //temp pin to double check the new pin to be stored
+    char tempPin[PIN_LENGTH + 1];
+
+    // Confirm New Pin
+    while (pinIndex < PIN_LENGTH)
+    {
+      char tempKey = keypadMgr.getKey();
+      if (tempKey >= '0' && tempKey <= '9')
+      {
+        tempPin[pinIndex++] = tempKey;
+        tempPin[pinIndex] = '\0';
+        display.showPinProgress(tempPin);
+      }
+      delay(50);
+    }
+
+    // Compare to see if Pins are the same
+    if (strcmp(tempPin, enteredPin) == 0)
+    {
+      pinStore.savePin(enteredPin);
+      display.showMessage("Pin Updated");
+    }
+    else
+    {
+      display.showMessage("Mismatch, please try again");
+    }
+
+    delay(2000);
+    display.clear();
+    pinIndex = 0;
+  }
+
+  else
+  {
+    display.showMessage("Access Denied");
+    failedAttempts++;
+  }
+
+  // Reset for next atempt
+  pinIndex = 0;
+  delay(1000);
+  display.clear();
 }
 
 /**
  * @brief Arduino setup function.
- * 
+ *
  * Initializes serial communication and the keypad hardware.
  */
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   keypadMgr.begin();
   display.begin();
@@ -68,99 +205,41 @@ void setup() {
 
 /**
  * @brief Arduino main loop function.
- * 
+ *
  * Continuously polls the keypad for key presses and logs them to the serial monitor.
  */
-void loop() {
+void loop()
+{
+
+  // check to see if user
+  CheckIfLockOut();
+
   char key = keypadMgr.getKey();
 
-  //if key is pressed
-  if(key != '\0') {
+  // if key is pressed
+  if (key != '\0')
+  {
 
-    if (key >= '0' && key <= '9') {
+    if (key >= '0' && key <= '9')
+    {
 
-      if (pinIndex < PIN_LENGTH) {
-        enteredPin[pinIndex] = key;
-        pinIndex++;
-        enteredPin[pinIndex] = '\0';  // Null-terminate after adding
+      handlePinEntry(key);
+
+      // when 4 digits are entered
+      if (pinIndex == PIN_LENGTH)
+      {
+        handlePinValidation();
+      }
       
-        display.showPinProgress(enteredPin);
-      }
-
-      //when 4 digits are entered
-      if(pinIndex == PIN_LENGTH) {
-        enteredPin[PIN_LENGTH] = '\0'; //null terminate string
-
-        //Compare if entered pin is the correct pin
-        if (strcmp(enteredPin, pinStore.getPin()) == 0) {
-          display.showMessage("Access Granted");
-          unlockLock();
-          locked = false;
-        } 
-
-        //pin overide option
-        else if(strcmp(enteredPin, "0101") == 0) {
-          display.showMessage("Enter a new pin to be saved");
-          pinIndex = 0;
-
-          //Enter new Pin
-          while (pinIndex < PIN_LENGTH) {
-            char tempKey = keypadMgr.getKey();
-            if (tempKey >= '0' && tempKey <= '9') {
-              enteredPin[pinIndex] = tempKey;
-              pinIndex++;
-              enteredPin[pinIndex] = '\0';
-              display.showPinProgress(enteredPin);
-            }
-            delay(50);
-          }
-
-          delay(500);
-          pinIndex = 0;
-          display.showMessage("Confirm pin");
-
-          char tempPin[PIN_LENGTH + 1];
-
-          //Confirm New Pin
-          while(pinIndex < PIN_LENGTH) {
-            char tempKey = keypadMgr.getKey();
-            if (tempKey >= '0' && tempKey <= '9') {
-              tempPin[pinIndex++] = tempKey;
-              tempPin[pinIndex] = '\0';
-              display.showPinProgress(tempPin); 
-            }
-            delay(50);
-          }
-
-          //Compare to see if Pins are the same
-          if(strcmp(tempPin, enteredPin) == 0) {
-            pinStore.savePin(enteredPin);
-            display.showMessage("Pin Updated");
-          } else {
-            display.showMessage("Mismatch, please try again");
-          }
-
-          delay(2000);
-          display.clear();
-          pinIndex = 0;
-        }
-
-        else {
-          display.showMessage("Access Denied");
-        }
-
-        //Reset for next atempt
-        pinIndex = 0;
-        delay(1000);
+      else if (key == '*')
+      {
+        // Reset user input on *
         display.clear();
+        pinIndex = 0;
       }
 
-    } else if (key == '*') {
-
-      //Reset user input on *
-      display.clear();
-      pinIndex = 0;
     }
+
+    delay(50);
   }
-  delay(50);
 }
